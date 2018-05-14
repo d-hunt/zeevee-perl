e5274bb93b2f0caa21b782ea8229f086cb9c4df1
Berk Akinci <bakinci@zeevee.com>
2018-05-14 16:32:15 -0400

    Adding Perl application that loads DP Transmitter firmware

---
+++ DPOut_DPTX_firmware_load.pl
@@ -0,0 +1,96 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+no warnings 'experimental::smartmatch';
+
+use lib '.'; # Some platforms (Ubuntu) don't search current directory by default.
+use ZeeVee::Aptovision_API;
+use ZeeVee::BlueRiverDevice;
+use ZeeVee::Apto_UART;
+use ZeeVee::DPGlueMCU;
+use Data::Dumper ();
+use Time::HiRes ( qw/sleep/ );
+use IO::Select;
+
+my $id_mode = "SINGLEDEVICE"; # Set to: SINGLEDEVICE, NEWDEVICE, HARDCODED
+my $device_id = 'd880399acbf4';
+my $host = '172.16.1.84';
+my $port = 6970;
+my $timeout = 10;
+my $debug = 1;
+my @output = ();
+my $json_template = '/\{.*\}\n/';
+
+my $apto = new ZeeVee::Aptovision_API( { Timeout => $timeout,
+					 Host => $host,
+					 Port => $port,
+					 JSON_Template => $json_template,
+					 Debug => $debug,
+				       } );
+
+# Determine the decoder to use for this test.
+if( $id_mode eq "NEWDEVICE" ) {
+    print "Waiting for a new decoder device...\n";
+    $device_id = $apto->wait_for_new_device("all_rx");
+} elsif ( $id_mode eq "SINGLEDEVICE" ) {
+    print "Looking for a single decoder.\n";
+    $device_id = $apto->find_single_device("all_rx");
+} elsif ( $id_mode eq "HARDCODED" ) {
+    print "Using hard-coded device: $device_id.\n";
+} else {
+    die "Unimlemented ID mode: $id_mode.";
+}
+print "Using decoder (DUT) $device_id.\n";
+
+my $decoder = new ZeeVee::BlueRiverDevice( { DeviceID => $device_id,
+					     Apto => $apto,
+					     Timeout => $timeout,
+					     VideoTimeout => 20,
+					     Debug => $debug,
+					   } );
+
+my $uart = new ZeeVee::Apto_UART( { Device => $decoder,
+				    Host => $host,
+				    Timeout => $timeout,
+				    Debug => $debug,
+				  } );
+
+my $glue = new ZeeVee::DPGlueMCU( { UART => $uart,
+				    Timeout => $timeout,
+				    Debug => $debug,
+				  } );
+
+# Preparing for non-blocking reads from STDIN.
+my $io_select = IO::Select->new();
+$io_select->add(\*STDIN);
+
+#Temporary result values.
+my $result;
+my $expected;
+
+print "BlueRiver Device Die Temperature: "
+    .$decoder->temperature()
+    ."\n";
+
+# Open and read file.
+my $filename = "Charlie_DP_EP196E.bin";
+my $flash_base = 0x0;
+my $max_filesize = (0x7800-0x1000);
+my $data_string = "";
+open( FILE, "<:raw", $filename )
+    or die "Can't open file $filename.";
+read( FILE, $data_string, $max_filesize )
+    or die "Error reading from file $filename.";
+close FILE;
+print "Read file $filename.  Length: ".length($data_string)." Bytes.\n";
+
+print "Updating EP196E MCU.\n";
+$glue->DPTX_program($flash_base, $data_string);
+print "Verifying EP196E MCU.\n";
+$glue->DPTX_verify($flash_base, $data_string)
+    or die "Read/Verify failed!";
+
+print "=== DONE. DeviceID = ".$decoder->DeviceID()."===\n";
+
+exit 0;
